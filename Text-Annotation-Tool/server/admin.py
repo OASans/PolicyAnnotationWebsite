67397f5d1291d8ec0e00127093ab2e6b937270b4
OASans <40996687+OASans@users.noreply.github.com>
2021-07-26 21:51:36 +0800

    Delete Text-Annotation-Tool directory

--- Text-Annotation-Tool/server/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Label, Document, Project
-from .models import DocumentAnnotation
-
-admin.site.register(DocumentAnnotation)
-admin.site.register(Label)
-admin.site.register(Document)
-admin.site.register(Project)
